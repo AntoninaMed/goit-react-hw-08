5a78d76fca5da9e9f4b7fc028110dca1003ac2d7
AntoninaMed <ton.kraski2427@gmail.com>
2024-05-19 17:44:00 +0300

    upd

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,11 +1,11 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { DocumentTitle } from "../components/DocumentTitle";
+import { DocumentTitle } from "../../components/App/DocumentTitle";
 import { ContactList } from "../components/ContactList/ContactList";
 import { fetchContacts } from "../redux/contacts/operations";
 import { selectIsLoading } from "../redux/contacts/selectors";
 import { ContactForm } from "../components/ContactForm/ContactForm";
-import { SearchBox } from "../components/SearchBox/SearchBox";
+import { SearchBox } from "../../components/SearchBox/SearchBox";
 
 const Contacts = () => {
   const dispatch = useDispatch();
